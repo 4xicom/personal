5dd4382ae09b202513b105d18100c7a9fb44bafe
Bo Zhang <zhangb@microsoft.com>
2024-10-11 11:35:50 -0700

    update

--- ports/entraidkafka/portfile.cmake
+++ ports/entraidkafka/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO 4xicom/personal
-    REF c690a29a856e9537ce6a63282424fa35d0bb666d
+    REF 4bbb4bcdb358339ffa0dcc2119292b329d8a7cb4
     SHA512 ca468b4db70e6dddcc034a5850ba0584858807708cea4380b3552ae5031863a084c6b0a582c560a362ddaa923d68dd96b3ec5365e98509dab330fdc60d81a148
     HEAD_REF master
 )
